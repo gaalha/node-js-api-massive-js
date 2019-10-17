326b3583c475d29788f8460491aea060bfaa49f5
DMGarcia17 <daniel.moreno17101998@gmail.com>
2019-10-17 09:10:13 -0600

    Added email validation for users sign up

--- app/controllers/api/userController.js
+++ app/controllers/api/userController.js
@@ -10,17 +10,21 @@ router.post(
         req.checkBody('txtIdUser').trim();
         req.checkBody('txtUsername').trim().notEmpty();
         req.checkBody('txtPassword').trim().notEmpty();
-        req.checkBody('txtEmail').trim();
+        req.checkBody('txtEmail').trim().notEmpty();
 
-        const [username, plainPassword, id, email]=[req.body.txtUsername, req.body.txtPassword, req.body.txtIdUser, req.body.txtEmail];
+        const [username, plainPassword, id, email] = [req.body.txtUsername, req.body.txtPassword, req.body.txtIdUser, req.body.txtEmail];
 
         let errors = req.validationErrors();
-
         if (errors) {
+            let message;
+            if (errors[0].param !== undefined && errors[0].param != null && errors[0].param.length > 0)
+                message = res.__('api.user.fields.empty.with.field') + errors[0].param;
+            else
+                res.__('api.user.fields.empty');
             res.send({
                 success: false,
-                message: res.__('api.user.fields.empty')
-            });
+                message
+                });
         } else {
             const isEditing = id !== undefined && id && id !== 0;
             const result = await userService.save(isEditing, id, username, plainPassword, email);
