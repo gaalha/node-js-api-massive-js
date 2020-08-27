9575734ac56206a0d20d7eafceb73d181f75e012
Edgar Mejia <edgar13155@gmail.com>
2020-08-27 09:53:49 -0600

    Add crud methods for user table.

--- app/controllers/api/userController.js
+++ app/controllers/api/userController.js
@@ -12,9 +12,8 @@ router.post(
         req.checkBody('email').trim().notEmpty();
         req.checkBody('password').trim().notEmpty();
 
-        const [id, user_name, email, password] = [req.body.id, req.body.user_name, req.body.email, req.body.password];
-
         let errors = req.validationErrors();
+
         if (errors) {
             let message;
             if (errors[0].param !== undefined && errors[0].param != null && errors[0].param.length > 0)
@@ -26,6 +25,7 @@ router.post(
                 message: message
             });
         } else {
+            const [id, user_name, email, password] = [req.body.id, req.body.user_name, req.body.email, req.body.password];
             const result = await userService.save(id, user_name, email, password);
 
             if (result) {
@@ -39,6 +39,48 @@ router.post(
                     message: id ? res.__('api.user.update.error') : res.__('api.user.save.error')
                 });
             }
+        }
+    })
+);
+
+router.get(
+    '/:id',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const result = await userService.getOne(req);
+
+        if (result.length === 0) { res.send({success:false, message:res.__('api.user.get.error')}); }
+        else { res.send({success: true, data: result}); }
+    })
+);
+
+router.get(
+    '*',
+    authJwt(),
+    asyncHandler(async (req, res) => {
+        const page = req.body.page || req.query.page || 1;
+        const pageSize = req.body.pageSize || req.query.pageSize || 10;
+        const result = await userService.getAll(req, page, pageSize);
+
+        if (result.length === 0) {
+            res.send({success:false, message:res.__('api.user.get.empty')});
+        } else {
+            let total = await userService.getAllCount();
+            res.send({success: true, data: result, total: total, pageSize, page: page});
+        }
+    })
+);
+
+router.delete(
+    '/delete/:id',
+    authJwt(),
+    asyncHandler(async (req, res, next) => {
+        const result = await userService.delete(req);
+
+        if (result!==null) {
+            res.send({success: true, message: res.__('api.user.delete.succes')});
+        } else {
+            res.send({success: false, message: res.__('api.user.delete.error')});
         }
     })
 );
--- app/services/userService.js
+++ app/services/userService.js
@@ -4,7 +4,7 @@ const CONSTANTS = require('../utils/constants');
 
 class userService {
     static async save(id, user_name, email, password) {
-        let user_app = {user_name};
+        let user_app = {user_name, email};
 
         if (id) {
             user_app.id = id;
@@ -12,10 +12,64 @@ class userService {
         } else {
             user_app.created_at = new Date();
         }
-        user_app.email = email;
+
         user_app.password = await bcrypt.hash(password, CONSTANTS.saltRounds); //.then(hash => {
         return db.pg.user_app.save(user_app);
     }
+
+    static async getOne(req) {
+        let id = req.params.id;
+        const result = await db.pg.user_app.findOne({id: id});
+        return result;
+    }
+
+    static async getAll(req, page, pageSize) {
+        let active = req.query.active || 'id';
+        const order = req.query.order || 'desc';
+        let search = req.body.search || req.query.search;
+        const newPage = (page -1) * pageSize;
+
+        if (search === undefined) {search = '%%';} 
+
+        const CRITERIA = {
+            'deleted_at IS': 'NULL',
+            or: [
+                {'user_name ILIKE': '%' + search + '%'},
+                {'email ILIKE': '%' + search + '%'}
+            ]
+        }
+
+        const result = await db.pg.user_app.find(
+            CRITERIA,
+            {
+                order: [{
+                    field: active,
+                    direction: order,
+                }],
+                offset: newPage,
+                limit: pageSize
+            }
+        )
+
+        return result;
+    }
+
+    static async getAllCount() {
+        let result = await db.pg.user_app.count({'deleted_at IS': 'NULL',});
+        return result;
+    }
+
+    static async delete(req) {
+        let id = req.params.id;
+        let user_app = {
+            id: id,
+            deleted_at: new Date()
+        }
+
+        const result = await db.pg.user_app.save(user_app);
+        return result;
+    }
+
 }
 
 module.exports = userService;
